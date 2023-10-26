f2f2f1e1440f97069c91356ac21bc689c5cbdfbb
gmartirosyan <grigor.martirosyan@jambit.com>
2023-10-26 13:15:09 +0400

    First commit message

---
+++ src/main/java/com/example/bitgithubmig/BitGithubMigApplication.java
@@ -0,0 +1,13 @@
+package com.example.bitgithubmig;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BitGithubMigApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BitGithubMigApplication.class, args);
+	}
+
+}
